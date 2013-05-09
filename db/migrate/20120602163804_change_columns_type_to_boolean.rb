c6d43eede8d05b05c0f14fd4dd3c1ba3eeb1e91f
Raul Cano <topolivan@gmail.com>
2013-05-09 18:24:08 +0200

    column type to boolean changed to PostGre compatible

--- db/migrate/20120602163804_change_columns_type_to_boolean.rb
+++ db/migrate/20120602163804_change_columns_type_to_boolean.rb
@@ -1,6 +1,18 @@
 class ChangeColumnsTypeToBoolean < ActiveRecord::Migration
-  def change
-    change_column :feed_source_entries, :is_read, :boolean
-    change_column :feed_source_entries, :is_starred, :boolean
+  def up
+    connection.execute(%q{
+      alter table feed_source_entries
+      alter column is_read
+      type boolean using cast(is_read as boolean)
+    })
+    connection.execute(%q{
+      alter table feed_source_entries
+      alter column is_starred
+      type boolean using cast(is_starred as boolean)
+    })
+  end
+  def down
+    change_column :feed_source_entries, :is_read, :integer
+    change_column :feed_source_entries, :is_starred, :integer
   end
 end
